c61795bd331709a45b6dc7b541484c27246fe531
Amaka <cejike.2202794@stu.edu.ng>
2025-07-15 22:46:34 +0100

    Added drag functionality

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useId } from 'react';
 import Navbar from '../components/Navbar';
 import HabitCard from '../components/HabitCard';
 import AddHabitModal from '../components/AddHabitModal';
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useId } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import styled from 'styled-components';
